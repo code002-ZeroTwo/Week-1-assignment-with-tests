a1c9822966825af867ffbb3133f0a7b9681e3941
siddhartha <siddharthpaudel41@gmail.com>
2023-06-15 13:32:56 +0545

    palindrome

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -5,9 +5,42 @@
   Once you've implemented the logic, test your code by running
   - `npm run test-palindrome`
 */
+function isLetter(character) {
+  return /^[a-zA-Z]$/.test(character);
+}
+
+function reverse(str){
+  return str.split('').reverse().join('');
+}
 
 function isPalindrome(str) {
-  return true;
+  // remove all the special characters and whitespaces as well 
+  for(let i of str){
+    if(!isLetter(i)){
+      str = str.replace(i,"");
+    }
+  }
+  // and check if given string is palindrome or not
+  let firsthalf;
+  let secondhalf;
+  let length = str.length;
+  if(length % 2 === 0){
+    let mid = length/2;
+    firsthalf = str.slice(0,mid);
+    secondhalf = str.slice(mid);
+  }
+  else{
+    let mid = Math.ceil(length/2);
+    firsthalf =str.slice(0,mid-1);
+    secondhalf = str.slice(mid);
+
+  }
+  if(firsthalf.toUpperCase() === reverse(secondhalf).toUpperCase()){
+    return true;
+  }
+  return false;
 }
+
+console.log(isPalindrome('Eva, can I see bees in a cave?'));
 
 module.exports = isPalindrome;
